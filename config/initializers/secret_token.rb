0bc67ff9a46d0f0dfda0efcf945d6f532a97cfbd
Neal Pisenti <npisenti@gmail.com>
2011-07-18 17:27:31 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenPhysics::Application.config.secret_token = '5da6e0228bc1f5dd4cc6621010e7d715b2147101e5368d3501d2c512b0c77b3e56bc189da90872f34dfe1192057d555a3c06eaf6cf4e7aaae810f8f51330a059'
