5e98ffc7eb3b3932d2cf270c5dea347810a40798
Neal Pisenti <npisenti@gmail.com>
2011-07-20 20:06:28 -0700

    added rudimentary permissions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,43 +1,38 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:edit]
-  # GET /users
-  # GET /users.xml
+  before_filter :authenticate, :only => [:edit, :show, :update, :destroy, :index]
+ 
   def index
     @users = User.all
-
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @users }
     end
   end
 
-  # GET /users/1
-  # GET /users/1.xml
   def show
     @user = User.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @user }
     end
   end
 
-  # GET /users/new
-  # GET /users/new.xml
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
     @user = User.find(params[:id])
+    if (@user != @current_user)
+      flash[:error] = "You don't have the right permissions!"
+      redirect_to users_path 
+      return
+    end
+
   end
 
-  # POST /users
-  # POST /users.xml
   def create
     @user = User.new(params[:user])
-
     respond_to do |format|
       if @user.save
         format.html { redirect_to(@user, :notice => 'User was successfully created.') }
@@ -49,10 +44,13 @@ class UsersController < ApplicationController
     end
   end
 
-  # PUT /users/1
-  # PUT /users/1.xml
   def update
     @user = User.find(params[:id])
+    if (@user != @current_user)
+      flash[:error] = "You don't have the right permissions!"
+      redirect_to users_path 
+      return
+    end
 
     respond_to do |format|
       if @user.update_attributes(params[:user])
@@ -65,12 +63,15 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.xml
   def destroy
+    if @current_user.perm != "admin"
+      flash[:error] = "You don't have permissions to do that!"
+      redirect_to users_path
+      return
+    end
+
     @user = User.find(params[:id])
     @user.destroy
-
     respond_to do |format|
       format.html { redirect_to(users_url) }
       format.xml  { head :ok }
